7efaf954370b32c2e00c110baf2eaf9d271f7705
mmrakt <mimuraakkun3@gmail.com>
2021-04-25 20:10:41 +0900

    dropdownメニュー実装

--- src/components/NavbarItem.tsx
+++ src/components/NavbarItem.tsx
@@ -1,43 +1,58 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Menu from 'material-ui-popup-state/HoverMenu'
+import MenuItem from '@material-ui/core/MenuItem'
+import {
+  usePopupState,
+  bindHover,
+  bindMenu,
+} from 'material-ui-popup-state/hooks'
 
 type IProps = {
   name: string
   to: string
   isDropdown?: boolean
 }
+
 const NavbarItem = (props: IProps) => {
   const { name, to, isDropdown } = props
-  const [isOpened, setIsOpened] = React.useState(false)
+  const popupState = usePopupState({ variant: 'popover', popupId: 'demoMenu' })
 
   return (
-    <div
-      className="navbar-item hover:bg-blue-300 flex-1 flex text-lg ext-white items-center justify-center"
-      onMouseOver={() => setIsOpened(true)}
-      onFocus={() => setIsOpened(true)}
-      onMouseOut={() => setIsOpened(false)}
-      onBlur={() => setIsOpened(false)}
-    >
+    <>
       {isDropdown ? (
         <>
-          <span>{name}</span>
-          {isOpened && (
-            <div>
-              <ul>
-                <li>カラーセラピー</li>
-                <li>パーソナルカラー診断</li>
-                <li>ワイヤービジューバッグ</li>
-                <li>パーソナルコーチング</li>
-              </ul>
-            </div>
-          )}
+          <div
+            className="navbar-item hover:bg-blue-300 flex-1 flex text-lg ext-white items-center justify-center"
+            /* eslint-disable react/jsx-props-no-spreading */ {...bindHover(
+              popupState
+            )}
+          >
+            {name}
+          </div>
+          <Menu
+            /* eslint-disable react/jsx-props-no-spreading */
+            {...bindMenu(popupState)}
+            getContentAnchorEl={null}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+            transformOrigin={{ vertical: 'top', horizontal: 'left' }}
+          >
+            <MenuItem onClick={popupState.close}>カラーセラピー</MenuItem>
+            <MenuItem onClick={popupState.close}>パーソナルカラー診断</MenuItem>
+            <MenuItem onClick={popupState.close}>
+              ワイヤービジューバッグ
+            </MenuItem>
+            <MenuItem onClick={popupState.close}>ダイエット</MenuItem>
+          </Menu>
         </>
       ) : (
-        <Link to={`/${to}`}>
-          <span>{name}</span>
-        </Link>
+        <div className="navbar-item hover:bg-blue-300 flex-1 flex text-lg ext-white items-center justify-center">
+          <Link to={`/${to}`}>
+            <span>{name}</span>
+          </Link>
+        </div>
       )}
-    </div>
+    </>
   )
 }
 
