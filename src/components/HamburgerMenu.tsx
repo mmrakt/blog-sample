992d146104161512b3509aaf04311ebcb54c5721
mmrakt <mimuraakkun3@gmail.com>
2021-04-29 12:39:20 +0900

    react-responsive導入

---
+++ src/components/HamburgerMenu.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MenuIcon from '@material-ui/icons/Menu'
+
+const HamburgerMenu: React.VFC = () => {
+  const hoge = ''
+
+  return (
+    <div className="className">
+      <MenuIcon />
+    </div>
+  )
+}
+
+export default HamburgerMenu
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import MediaQuery from 'react-responsive'
 import Head from './Head'
 import Footer from './Footer'
 import Navbar from './Navbar'
+import HamburgerMenu from './HamburgerMenu'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Head />
     <div className="mx-auto md:w-768">
-      <Navbar />
+      <MediaQuery query="(max-width: 360px)"><HamburgerMenu /></MediaQuery>
+      <MediaQuery query="(min-width: 361px)"><Navbar /></MediaQuery>
+      
       <main className="">{children}</main>
       <hr />
       <Footer />
