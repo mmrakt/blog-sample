75f68668765212c36a16522f6dd7905787826f28
mmrakt <mimuraakkun3@gmail.com>
2021-09-13 23:42:31 +0900

    modify 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
-import Layout from '../components/Layout'
+import MediaQuery from 'react-responsive'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  </Layout>
+  <>
+    <Header />
+    <main className="bg-gray-100 px-3 py-10">
+      <h1 className="text-3xl text-center">
+        お探しのページが見つかりませんでした
+      </h1>
+      <div className="mt-10 text-center text-lg">
+        <MediaQuery maxWidth={980}>
+          <p>
+            アクセスしたページは削除またはURLが変更されていたため表示することができませんでした。
+          </p>
+        </MediaQuery>
+        <MediaQuery minWidth={981}>
+          <p>アクセスしたページは削除またはURLが変更されていたため</p>
+          <p>表示することができませんでした。</p>
+        </MediaQuery>
+      </div>
+    </main>
+    <Footer />
+  </>
 )
 
 export default NotFoundPage
